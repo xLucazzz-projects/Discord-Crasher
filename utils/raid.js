7d4c8c6809a98ba4bb5e4765f95c001fd2986f5b
xLucazzz-projects <69597508+xLucazzz-projects@users.noreply.github.com>
2020-11-06 09:07:01 -0300

    added delay

--- utils/raid.js
+++ utils/raid.js
@@ -3,7 +3,8 @@ const colors = require('colors')
 
 var mensagens = 0
 
-async function raid(token, id) {
+
+async function raid(token, id, delay) {
     var emojis = ["\uD83D\uDE01", "\uD83D\uDE03", "\uD83D\uDE04", "\uD83D\uDE05", "\uD83D\uDE06", "\uD83D\uDE09", "\uD83D\uDE0A", "\uD83D\uDE0B", "\uD83D\uDE0C", "\uD83D\uDE0D", "\uD83D\uDE0F", "\uD83D\uDE12", "\uD83D\uDE13", "\uD83D\uDE14", "\uD83D\uDE16", "\uD83D\uDE18", "\uD83D\uDE1A", "\uD83D\uDE1C", "\uD83D\uDE1D", "\uD83D\uDE1E", "\uD83D\uDE20", "\uD83D\uDE21", "\uD83D\uDE22", "\uD83D\uDE23", "\uD83D\uDE24", "\uD83D\uDC17", "\uD83D\uDC18", "\uD83D\uDC19", "\uD83D\uDC11", "\uD83D\uDC0E", "\uD83D\uDC0D", "\uD83D\uDC0C", "\uD83C\uDFF0"]
     var message = `**`
     for (i = 0; i < 1995; i++) {
@@ -13,15 +14,17 @@ async function raid(token, id) {
             message += `${emojis[Math.floor(Math.random() * emojis.length)]}`
         }
     }
+    
     await axios.post(`https://discord.com/api/v8/channels/${id}/messages`, { "content": `${message}` }, {
         headers: {
             "authorization": `${token}`
         }
-    }).then(response => {
+    }).then(async response => {
         if (response.data.id) {
             mensagens++
-            console.log(colors.green(`[+] `) + `${mensagens} mensagens enviadas!`)
-            raid(token, id)
+            console.log(colors.green(`[+] `) + `${mensagens} mensagens enviadas!` + colors.yellow(` [Awaiting ${delay}ms]`))
+            await sleep(delay)
+            raid(token, id, delay)
         } else {
             console.log(colors.red(`[-] `) + `Ocorreu um erro desconhecido ao enviar a mensagem`)
         }
@@ -29,12 +32,19 @@ async function raid(token, id) {
         if (err.response.status === 429) {
             console.log(colors.red(`[RATE LIMIT] Awaiting ${err.response.data.retry_after.toString().replace('.', '')}ms`))
             setTimeout(() => {
-                raid(token, id)
+                raid(token, id, delay)
             }, err.response.data.retry_after.toString().replace('.', ''));
         } else {
             console.log(colors.red(`[SPAMMER] Ocorreu um erro ao enviar a mensagem. Error: ${err}`))
         }
     })
+
+}
+
+async function sleep(duration) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), duration);
+    });
 }
 
 module.exports = raid;
